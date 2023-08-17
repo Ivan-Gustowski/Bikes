3b4290a652e5c41c30a2255c616f2ebe29837a73
Ivan Gustowski <gustowskiivan@gmail.com>
2023-08-17 14:53:18 -0300

    Final

---
+++ js/bicicletas.js
@@ -0,0 +1,23 @@
+/* Esta lista podría ser reemplazada por la respuesta de un backend */
+const bicicletas = [
+    {
+      id:1,
+      nombre: "SAVA",
+      precio: 825000,
+    },
+    {
+      id:2,
+      nombre: "WILIER",
+      precio: 140000,
+    },
+    {
+      id:3,
+      nombre: "ZION",
+      precio: 162000,
+    },
+    {
+      id:4,
+      nombre: "VOLTA",
+      precio: 500000,
+    }
+  ]
